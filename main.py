39d5136f1b64c0f87841a267be8729cdbea4fcae
Fernando Valverde <fernando@fdo.cr>
2023-04-11 18:12:28 +0100

    Add pid.txt usage

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import logging
 import time
+import os
+
 from dotenv import load_dotenv
 load_dotenv()
-
-import os
 trigger_url = os.environ.get("TRIGGER_URL")
+
+# Write pid to file so we can kill it if needed
+f = open("pid.txt", "w")
+f.write(str(os.getpid()))
+f.close()
 
 import requests
 from gpiozero import MotionSensor
 pir = MotionSensor(4)
-timeout = 20
+timeout = 45
 motion_time = time.time() - timeout
 
 while True:
@@ -18,6 +23,8 @@ while True:
         print("Motion detected")
         res = requests.get(trigger_url)
         print("Trigger request response ", res.status_code)
+    else:
+        print("Motion detected (inside timeout)")
     motion_time = time.time()
     pir.wait_for_no_motion()
 
